b5a11841f70181a0ee000c317a578081dbaee0d6
warrior14 <ironaddicts14@gmail.com>
2021-09-06 14:39:09 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BankHeist 
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine();
+        }
+    }
+}
